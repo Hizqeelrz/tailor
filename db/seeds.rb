ec874e1b96dd7a121ec6a67022d6f9b770b2c65a
hizqeelrz <hizqeel1214@gmail.com>
2017-09-17 18:29:15 +0500

    seed for role and dashboard add

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 
 User.delete_all
 User.create(email: "user1@tailor.com", password: "password", password_confirmation: "password", role: "chief_designer")
+
+User.create(email: "user2@tailor.com", password: "password", password_confirmation: "password", role: "gents_master")
+
+User.create(email: "user3@tailor.com", password: "password", password_confirmation: "password", role: "ladies_master")
+
+User.create(email: "user4@tailor.com", password: "password", password_confirmation: "password", role: "tailor")
